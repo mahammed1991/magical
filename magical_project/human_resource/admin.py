06074b5083eb9b30ffc8111167aecd2156aa4d5a
mahammed1991 <ashuindia1991@gmail.com>
2018-01-17 12:46:55 +0530

    comments added in admin panal

--- magical_project/human_resource/admin.py
+++ magical_project/human_resource/admin.py
@@ -9,6 +9,7 @@ class EmployeeDetailsAdmin(admin.ModelAdmin):
 
     list_display = ('company','name','age','role')
 
+#Below by registering we are telling to django to show in admin panal
 admin.site.register(Company)
 admin.site.register(EmployeeDetails,EmployeeDetailsAdmin)
 admin.site.register(JobOpenings)
