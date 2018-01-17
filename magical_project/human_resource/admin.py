57338ff8377cf17d54c9205f46060e4fe96454c0
mahammed1991 <ashuindia1991@gmail.com>
2018-01-17 12:33:25 +0530

    admin changes

--- magical_project/human_resource/admin.py
+++ magical_project/human_resource/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Company, EmployeeDetails, JobOpenings
 
+#admin functionality goes here
+
+class EmployeeDetailsAdmin(admin.ModelAdmin):
+
+    list_display = ('company','name','age','role')
+
 admin.site.register(Company)
-admin.site.register(EmployeeDetails)
+admin.site.register(EmployeeDetails,EmployeeDetailsAdmin)
 admin.site.register(JobOpenings)
